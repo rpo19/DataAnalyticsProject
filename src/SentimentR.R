c368f81702b6e80bc9f340c79acfb6089fcd4209
christianbernasconi96 <christian.bernasconi96@gmail.com>
2020-06-22 16:38:07 +0200

    csv stuffs to fix...

--- src/SentimentR.R
+++ src/SentimentR.R
@@ -1,12 +1,36 @@
 library(sentix)
 
+
+
+compute_sentix <- function(df, model) {
+  df_polarity = sentix.df.sentiment(df$body, model)
+  df_polarity$doc_id = factor(df_polarity$doc_id)
+  df$X_id = NULL
+  ret = data.frame(df)
+  i = 1
+  for (level in levels(df_polarity$doc_id)) {
+    ret[i, 'polarity'] = head(df_polarity[df_polarity$doc_id == level, 'sentimentXdoc'], 1)$sentimentXdoc
+    i = i + 1
+  }  
+  
+  return(ret)
+}
+
+# load model
 model <- load.udpipe()
 
-dataReviews_NEG = read.csv("../data/dataReviews_NEG.csv")  # read csv file 
-sentiment_NEG = sentix.df.sentiment(dataReviews_NEG$body, model)
+# compute negative 
+dataReviews_NEG = read.csv("../data/dataReviews_NEG.csv", stringsAsFactors=FALSE, sep = '\t')  # read csv file 
+sentiment_NEG = compute_sentix(dataReviews_NEG, model)
+write.csv(sentiment_NEG, "../data/dataReviews_NEG_pol.csv", row.names=FALSE, sep = '\t')
 
-dataReviews_POS = read.csv("../data/dataReviews_POS.csv")  # read csv file 
-sentiment_POS = sentix.df.sentiment(dataReviews_POS$body, model)
+# compute positive
+dataReviews_POS = read.csv("../data/dataReviews_POS.csv", stringsAsFactors=FALSE, sep = '\t')  # read csv file 
+sentiment_POS = compute_sentix(dataReviews_POS, model)
+write.csv(sentiment_POS, "../data/dataReviews_POS_pol.csv", row.names=FALSE)
 
-dataReviews_NEUT = read.csv("../data/dataReviews_NEUT.csv")  # read csv file 
-sentiment_NEUT = sentix.df.sentiment(dataReviews_NEUT$body, model)
+# compute neutral
+dataReviews_NEUT = read.csv("../data/dataReviews_NEUT.csv", stringsAsFactors=FALSE)  # read csv file 
+sentiment_NEUT = compute_sentix(dataReviews_NEUT, model)
+write.csv(sentiment_NEUT, "../data/dataReviews_NEUT_pol.csv", row.names=FALSE)
+
