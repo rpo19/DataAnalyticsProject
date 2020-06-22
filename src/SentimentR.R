9b412a59a83333750860f9a442685bbf967b2b15
christianbernasconi96 <christian.bernasconi96@gmail.com>
2020-06-22 12:44:10 +0200

    fixed read csv

--- src/SentimentR.R
+++ src/SentimentR.R
@@ -5,8 +5,8 @@ model <- load.udpipe()
 dataReviews_NEG = read.csv("../data/dataReviews_NEG.csv")  # read csv file 
 sentiment_NEG = sentix.df.sentiment(dataReviews_NEG$body, model)
 
-dataReviews_POS = read.csv("../data/dataReviews_POS")  # read csv file 
+dataReviews_POS = read.csv("../data/dataReviews_POS.csv")  # read csv file 
 sentiment_POS = sentix.df.sentiment(dataReviews_POS$body, model)
 
-dataReviews_NEUT = read.csv("../data/dataReviews_NEUT")  # read csv file 
+dataReviews_NEUT = read.csv("../data/dataReviews_NEUT.csv")  # read csv file 
 sentiment_NEUT = sentix.df.sentiment(dataReviews_NEUT$body, model)
