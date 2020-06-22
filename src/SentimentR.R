7cf91936549bbf59c0fbe8839a449a9807830a7a
christianbernasconi96 <christian.bernasconi96@gmail.com>
2020-06-22 12:40:49 +0200

    sentix in R

---
+++ src/SentimentR.R
@@ -0,0 +1,12 @@
+library(sentix)
+
+model <- load.udpipe()
+
+dataReviews_NEG = read.csv("../data/dataReviews_NEG.csv")  # read csv file 
+sentiment_NEG = sentix.df.sentiment(dataReviews_NEG$body, model)
+
+dataReviews_POS = read.csv("../data/dataReviews_POS")  # read csv file 
+sentiment_POS = sentix.df.sentiment(dataReviews_POS$body, model)
+
+dataReviews_NEUT = read.csv("../data/dataReviews_NEUT")  # read csv file 
+sentiment_NEUT = sentix.df.sentiment(dataReviews_NEUT$body, model)
