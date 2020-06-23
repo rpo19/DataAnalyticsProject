ae6389904e2869e48d6dc6317e25449d4edccf42
rpo19 <pozzir00@gmail.com>
2020-06-23 18:36:08 +0200

    fix b64 ascii, example wordcloud dash

--- src/webapp/pages/sentiment.py
+++ src/webapp/pages/sentiment.py
@@ -1,7 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+
+communities_stats = pd.read_csv("../dataApp/communities_stats.csv", sep='\t')
+
+b64_image = communities_stats.iloc[0]["wordclouds"]
 
 sentiment_layout = [html.Div(children=[
-    html.H1(children='Sentiment')
+    html.H1(children='Sentiment'),
+    html.Img(src='data:image/png;base64,{0}'.format(b64_image))
 ])]
